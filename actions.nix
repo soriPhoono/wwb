5297d56bc488f8bdaa9cad130fa6bfdad4c6a9ab
soriphoono <soriphoono@gmail.com>
2026-04-27 10:25:30 -0500

    chore: bump frontend to 0.1.2 and finalize theme-aware AdminPage

--- actions.nix
+++ actions.nix
@@ -70,7 +70,7 @@ _: {
                   type=ref,event=branch
                   type=ref,event=pr
                   type=sha
-                  type=raw,value=0.1.1
+                  type=raw,value=0.1.2
                   type=raw,value=latest,enable=''${{ github.ref == 'refs/heads/main' }}
                 '';
               };
